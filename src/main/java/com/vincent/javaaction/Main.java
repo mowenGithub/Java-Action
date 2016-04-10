e47a24350b6be48689edf5a743aa3bcfb04543ab
wenlong.mo <1245146613@qq.com>
2016-04-10 10:12:09 +0800

    init project

---
+++ src/main/java/com/vincent/javaaction/Main.java
@@ -0,0 +1,11 @@
+package com.vincent.javaaction;
+
+/**
+ * Created by mowen on 4/10/16.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("init project");
+    }
+}
